30790162dfe6c3d0971316b549bb2ab6abf8e157
Carlos Soria <carlossoria1999@gmaill.com>
2022-10-24 11:07:34 -0500

    Creacion de la app login

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from ast import If
 from curses import use_default_colors
 
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -37,3 +38,20 @@ def signup(request):
             'form': UserCreationForm,
             'error': 'las contraseñas no coinciden'
         })
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            "form": AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST
+            ['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                "form": AuthenticationForm, 
+                "error": "Username or password is incorrect."
+            })
+
+        return redirect('home')
