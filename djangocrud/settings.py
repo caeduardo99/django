65394be797f8cef9602e4af10c1882f70d783699
Carlos Soria <78270693+caeduardo99@users.noreply.github.com>
2023-01-17 22:25:30 -0500

    Update settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -130,8 +130,3 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
